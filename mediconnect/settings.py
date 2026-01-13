f4b58f8f88a19431dfcfc8415ecaa06376ce2468
dikshathakre18 <dthakre709@gmail.com>
2026-01-14 01:55:58 +0530

    Fix static files and prepare for Render

--- mediconnect/settings.py
+++ mediconnect/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,18 +118,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# ================= STATIC FILES =================
 
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'core/static',
+    BASE_DIR / 'core' / 'static',
 ]
 
-# Use our custom login URL so @login_required redirects to '/login/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# ================= AUTH REDIRECTS =================
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
